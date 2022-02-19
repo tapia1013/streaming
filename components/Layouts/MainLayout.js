56075890ecdac0f16a1cd3415830e9ba2a55e9eb
tapia1013 <manbearpig1013@gmail.com>
2022-02-18 20:20:33 -0800

    create side nav

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -4,14 +4,14 @@ import Header from "../UI/Header";
 
 const MainLayout = (props) => {
   return (
-    <>
+    <div style={{ background: 'linear-gradient(135deg, rgba(2,27,64,1) 11%, rgba(119,30,135,1) 100%)', minHeight: '100vh' }}>
       <Header />
       <section className="content-cotnainer">
         {props.children}
       </section>
-    </>
+    </div>
   )
 }
 
-
+// 5. Create Side Nav @0:38sec
 export default MainLayout;
--- components/UI/Header.js
+++ components/UI/Header.js
@@ -2,8 +2,23 @@
 
 const Header = (props) => {
   return (
-    <header>
-      Header
+    <header className='top-header'>
+      <div className="top-header__left-side">
+        <div className="top-header__menu-btn">
+          <i className="fas fa-bars" />
+        </div>
+
+        <div className="top-header__search-btn">
+          <i className="fas fa-search" />
+        </div>
+      </div>
+
+      <div className="top-header__logo"></div>
+
+      <div className="top-header__account">
+        <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/small-white-dog-breeds-maltese-1560293184.jpg?crop=1.00xw:0.663xh;0,0.337xh&resize=480:*' className="top-header__user-img" />
+        <div className="top-header__user-name">Cookie</div>
+      </div>
     </header>
   )
 }
